8d6e06a35ee3c72e9d32fccfbadc3a94837e9b68
lipocodes <lipocodes@yahoo.com>
2025-03-23 13:14:23 +0200

    commit146

--- main.js
+++ main.js
@@ -81,5 +81,5 @@ function create_link(journal){
  words = words.filter(word => word !== ''); 
  //Join the words with hyphens
  let formattedString = words.join('-'); 
- return `<a href="https://research.com/journal/${formattedString}">journal</a>`
+ return `<a href="https://research.com/journal/${formattedString}">${journal}</a>`
 }

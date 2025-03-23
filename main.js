6b19c80154a50886b49a148c98037d5d5e2d26f5
lipocodes <lipocodes@yahoo.com>
2025-03-23 15:34:27 +0200

    commit220

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ document.getElementById('searchInput').addEventListener('input', function() {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function create_link(journal) {
+function create_link(journal) {
     // Remove colons and ampersands, and convert to lowercase
     let cleanedString = journal
     .replace(/[:&]/g, '') // Remove colons and ampersands
